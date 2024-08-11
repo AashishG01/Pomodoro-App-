c57f0666d6aa2e72bfeafe2093f1e93443499daf
Aashish Gupta <117626035+AashishG01@users.noreply.github.com>
2024-08-11 10:42:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,100 @@
+from tkinter import *
+import math
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+timer = ""
+
+
+# ---------------------------- TIMER RESET ------------------------------- #
+
+def reset_timer():
+    global timer
+    global reps
+    window.after_cancel(timer)
+    count_label.config(text="")
+    canvas.itemconfig(timer_text, text=f"00:00")
+    timer_label.config(text="Timer")
+    reps = 0
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        timer_label.config(text="Long break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        timer_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    global timer
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    if count_min < 10:
+        count_min = f"0{count_min}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        mark = ""
+        work_sessions = math.floor(reps / 2)
+        for _ in range(work_sessions):
+            mark += "✓"
+        count_label.config(text=mark)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+# timer label
+timer_label = Label(text="Timer", font=(FONT_NAME, 22, "bold"), fg=GREEN)
+timer_label.config(bg=YELLOW, )
+timer_label.grid(column=1, row=0)
+# Tomato ui
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+# count
+# count_down(5)
+# start button
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
+start_button.grid(column=0, row=2)
+
+# Reset button
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
+reset_button.grid(column=2, row=2)
+
+# time label + counter
+count_label = Label(font=(FONT_NAME, 15, "bold"), fg=GREEN)
+count_label.config(bg=YELLOW)
+count_label.grid(column=1, row=3)
+
+window.mainloop()
